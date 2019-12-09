b9de6442863c8fe16d42bb5f2be30895845c34f6
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2019-12-09 16:19:31 +0100

    Scroll to the last node in the list on page update.

--- src/components/JokeList.svelte
+++ src/components/JokeList.svelte
@@ -1,28 +1,40 @@
 <script lang="ts">
-  import { fade } from "svelte/transition";
-  import { get } from "svelte/store";
-  // @ts-ignore
-  import { jokes, favourites } from "../store.ts";
-  import JokeItem from "./JokeItem.svelte";
-  // @ts-ignore
-  import { Joke } from "../types.ts";
-  import { checkIfSaved } from "./ActionsLevel.svelte";
+    import { afterUpdate } from 'svelte'
+    import { fade } from 'svelte/transition'
+    import { get } from 'svelte/store'
+    // @ts-ignore
+    import { jokes, favourites } from '../store.ts'
+    import JokeItem from './JokeItem.svelte'
+    // @ts-ignore
+    import { Joke } from '../types.ts'
+    import { checkIfSaved } from './ActionsLevel.svelte'
 
-  const jokesFromStore: Joke[] = get(jokes);
-  const favouriteJokes: Joke[] = get(favourites);
+    const jokesFromStore: Joke[] = get(jokes)
+    const favouriteJokes: Joke[] = get(favourites)
 
-  const checkedJokes: Joke[] = jokesFromStore.map(joke =>
-    checkIfSaved(favouriteJokes, joke)
-  );
-  jokes.set(checkedJokes);
+    const checkedJokes: Joke[] = jokesFromStore.map(joke =>
+        checkIfSaved(favouriteJokes, joke)
+    )
+    jokes.set(checkedJokes)
+
+    let jokeNode: HTMLElement
+
+    afterUpdate(() => {
+      if (jokeNode) {
+        jokeNode.scrollIntoView({ behavior: 'smooth' })
+      }
+    })
 </script>
 
 <div class="box">
-  <div id="jokes" class="list">
-    {#each $jokes as item, index (item.id)}
-      <div in:fade={{ duration: 250 }} out:fade={{ duration: 250 }}>
-        <JokeItem {item} {index} />
-      </div>
-    {/each}
-  </div>
+    <div id="jokes" class="list">
+        {#each $jokes as item, index (item.id)}
+            <div
+                in:fade={{ duration: 250 }}
+                out:fade={{ duration: 250 }}
+                bind:this={jokeNode}>
+                <JokeItem {item} {index} />
+            </div>
+        {/each}
+    </div>
 </div>
